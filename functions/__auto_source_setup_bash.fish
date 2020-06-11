917a1ccdaa9e91754a615fb54e66557059690e51
Kenji Miyake <kenji.miyake@tier4.jp>
2020-06-12 08:30:33 +0900

    Prevent from multiple executions
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- functions/__auto_source_setup_bash.fish
+++ functions/__auto_source_setup_bash.fish
@@ -1,5 +1,6 @@
 function __auto_source_setup_bash
     set -q DISABLE_AUTO_SOURCE && return
+    set -q AUTO_SOURCE_EXECUTED && return
 
     set -l directories "devel" "install"
 
@@ -7,6 +8,7 @@ function __auto_source_setup_bash
         if test -e $directory/setup.bash
             echo "source $directory/setup.bash"
             bass source $directory/setup.bash
+            set -g AUTO_SOURCE_EXECUTED
             return
         end
     end
