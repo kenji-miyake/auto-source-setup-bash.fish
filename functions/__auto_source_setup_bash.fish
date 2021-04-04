7c1a24d2b870bfbb71814e177be7ee2cde10a8c1
Kenji Miyake <kenji.miyake@tier4.jp>
2021-04-04 10:46:28 +0900

    Add as command
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- functions/__auto_source_setup_bash.fish
+++ functions/__auto_source_setup_bash.fish
@@ -2,17 +2,5 @@ function __auto_source_setup_bash
     set -q AUTO_SOURCE_DISABLED && return
     set -q AUTO_SOURCE_EXECUTED && return
 
-    set setup_bash (__auto_source_find_setup_bash)
-
-    if $AUTO_SOURCE_USE_PREV_SETUP_BASH && test (pwd) = "$HOME" && not test -e "$setup_bash"
-        set setup_bash $AUTO_SOURCE_PREV_SETUP_BASH
-    end
-
-    test -e "$setup_bash" || return
-
-    set -U AUTO_SOURCE_PREV_SETUP_BASH $setup_bash
-
-    echo "source $setup_bash"
-    bass source $setup_bash
-    set -g AUTO_SOURCE_EXECUTED
+    as
 end
---
+++ functions/as.fish
@@ -0,0 +1,15 @@
+function as
+    set setup_bash (__auto_source_find_setup_bash)
+
+    if $AUTO_SOURCE_USE_PREV_SETUP_BASH && test (pwd) = "$HOME" && not test -e "$setup_bash"
+        set setup_bash $AUTO_SOURCE_PREV_SETUP_BASH
+    end
+
+    test -e "$setup_bash" || return
+
+    set -U AUTO_SOURCE_PREV_SETUP_BASH $setup_bash
+
+    echo "source $setup_bash"
+    bass source $setup_bash
+    set -g AUTO_SOURCE_EXECUTED
+end
